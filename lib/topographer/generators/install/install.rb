5dc51c3f83f9ba80126ee628725b791b10b5dd83
Emerson Huitt <emerson.huitt@scimedsolutions.com>
2014-04-18 14:15:27 -0400

    Finish generator
    Added remaining templates and tasks to the install Thor task.  This task can be invoked after installing the Topographer gem by running `thor topographer:install` in your project root folder.  If you want to install mapping templates for any models, please specify them using the `--models` option followed by a space delimited list of models to generate templates for.

--- lib/topographer/generators/install/install.rb
+++ lib/topographer/generators/install/install.rb
@@ -11,19 +11,28 @@ module Topographer::Generators
       File.expand_path("../../templates", __FILE__)
     end
 
+    def ignored_strategy_symbols
+      [:Base, :ImportStatus]
+    end
+
     def add_strategies
       strategy_module = Topographer::Importer::Strategy
       strategy_files = []
+      @strategy_classes = []
+      @strategy_class_names = []
+      @namespace_files = []
 
       strategy_module.constants(false).each do | constant_symbol |
-        if constant_symbol != :Base
+        unless ignored_strategy_symbols.include?(constant_symbol)
           constant = strategy_module.const_get( constant_symbol )
           if constant.is_a? Class
-
+            @strategy_classes << constant
+            @strategy_class_names << constant_symbol.to_s
             filename = "#{underscore_name(constant_symbol.to_s)}.rb"
-            filepath = strategy_path(filename)
 
-            strategy_files << File.join('strategy/', filename)
+            strategy_path = subdirectory_path('strategy/', filename)
+            filepath = import_path(strategy_path)
+            strategy_files << strategy_path
 
             template('strategy_class.rb.erb', filepath, {
               class_name: constant_symbol.to_s,
@@ -32,30 +41,96 @@ module Topographer::Generators
           end
         end
       end
-
-      template('namespace_module.rb.erb', import_path('strategy.rb'), {
-        namespace_files: strategy_files,
-        module_name: 'Strategy'
-      })
+      add_namespace_module('Strategy', strategy_files)
     end
 
     def add_mappings
-      mapping_files = []
+      mapping_base_path = subdirectory_path('mappings/', 'base.rb')
+      mapping_files = [mapping_base_path]
 
-      #TODO generate mapping files here for models passed in as args
+      config_file = File.join(destination_root, 'config/environment.rb')
+      is_rails = File.exists?(config_file)
 
-      template('namespace_module.rb.erb', import_path('mappings.rb'), {
-        namespace_files: mapping_files,
-        module_name: 'Mappings'
+      if is_rails
+        require_relative config_file
+        require 'active_support'
+        options[:models].each do |model|
+          model_class = model.classify.constantize
+
+          model_path = subdirectory_path('mappings/', "#{model.downcase}.rb")
+          filepath = import_path(model_path)
+          mapping_files << model_path
+
+          template('mapping.rb.erb', filepath, {
+            model_class: model_class,
+            strategy_classes: @strategy_classes
+          })
+        end
+        template('rails_mapping_base.rb.erb', import_path(mapping_base_path))
+      else
+        say 'Unable to generate model mappings outside of a Rails project'
+
+        template('mapping_base.rb.erb', import_path(mapping_base_path))
+      end
+
+      add_namespace_module('Mappings', mapping_files)
+    end
+
+    def add_runners
+      runner_base_path = subdirectory_path('runners/', 'base.rb')
+      runner_files = [runner_base_path]
+
+      template('runner_base.rb.erb', import_path(runner_base_path))
+
+      @strategy_class_names.each do |strategy_class|
+        filename = "#{underscore_name(strategy_class)}.rb"
+
+        runner_path = subdirectory_path('runners/', filename)
+        filepath = import_path(runner_path)
+        runner_files << runner_path
+
+        template('runner.rb.erb', filepath, {
+          strategy_class: strategy_class,
+          import_name: underscore_name(strategy_class)
+        })
+      end
+
+      add_namespace_module('Runners', runner_files)
+    end
+
+    def add_imports
+      import_base_path = subdirectory_path('.','base.rb')
+      ui_import_path = subdirectory_path('.', 'user_interface_import.rb')
+      commandline_import_path = subdirectory_path('.', 'commandline_import.rb')
+
+      @namespace_files += [import_base_path, ui_import_path, commandline_import_path]
+
+      template('import_base.rb.erb', import_path(import_base_path))
+      template('user_interface_import.rb.erb', import_path(ui_import_path))
+      template('commandline_import.rb.erb', import_path(commandline_import_path))
+      template('imports.rb.erb', File.join('./lib/', 'imports.rb'), {
+        namespace_files: @namespace_files
       })
+
+      template('log_display.erb', File.join('./app/views/shared', '_import_log_display.html.erb'))
     end
 
     private
-      def strategy_path(filename)
-        File.join('./lib/imports/strategy/', filename)
+      def subdirectory_path(subdirectory, filename)
+        File.join(subdirectory, filename)
       end
       def import_path(filename)
         File.join('./lib/imports/', filename)
+      end
+
+      def add_namespace_module(module_name, files)
+        module_path = subdirectory_path('.', "#{module_name.downcase}.rb")
+        @namespace_files << module_path
+
+        template('namespace_module.rb.erb', import_path(module_path), {
+          namespace_files: files,
+          module_name: module_name
+        })
       end
   end
 end
---
+++ lib/topographer/generators/templates/commandline_import.rb.erb
@@ -0,0 +1,6 @@
+class Imports::CommandLineImport
+  extend Imports::Base
+  def self.import_spreadsheet(file, import_runner_class, import_class, options = {})
+    run_import(file, import_class, import_runner_class, options)
+  end
+end
